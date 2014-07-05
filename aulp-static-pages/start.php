93ec43df975a4435b54715af68efc74f746800de
Maximilian Friedersdorff <maxf130@gmail.com>
2014-07-05 13:56:30 +0100

    Add contact and about page
    Plugin now renders about at '/about' and contact at '/contact'.  Still only
    dummy text until AULP supplies other stuff.

--- aulp-static-pages/start.php
+++ aulp-static-pages/start.php
@@ -14,9 +14,34 @@ function aulp_static_pages_init(){
     
     // Register plugin hook to render home page
     elgg_register_plugin_hook_handler('index', 'system', 'aulp_home');
+    
+    
+    // Register page handlers for contact and about
+    elgg_register_page_handler('contact', 'aulp_contact');
+    elgg_register_page_handler('about', 'aulp_about');
 }
 
 function aulp_home(){
-    include_once $_SERVER['DOCUMENT_ROOT'] . "/mod/aulp-static-pages/pages/index.php";
+    // Ensure page loads, otherwise return false to indicate 404
+    if(!include_once $_SERVER['DOCUMENT_ROOT'] . "/mod/aulp-static-pages/pages/index.php")
+    {
+        return false;
+    }
+    return true;
+}
+
+function aulp_contact(){
+    // Ensure page loads, otherwise return false to indicate 404
+    if(!include_once $_SERVER['DOCUMENT_ROOT'] . "/mod/aulp-static-pages/pages/contact.php"){
+        return false;
+    }
+    return true;
+}
+
+function aulp_about(){
+    // Ensure page loads, otherwise return false to indicate 404
+    if(!include_once $_SERVER['DOCUMENT_ROOT'] . "/mod/aulp-static-pages/pages/about.php"){
+        return false;
+    }
     return true;
 }
