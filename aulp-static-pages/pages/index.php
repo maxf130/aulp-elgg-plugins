b12ce11af67d39f05b0318199374c3919380aca0
Maximilian Friedersdorff <maxf130@gmail.com>
2014-07-05 13:40:40 +0100

    Remap homepage to custom
    Homepage is defined in pages/index.php  Still only displaying dummy text.  Will be
    changed as soon as AULP sends proper contnent.

---
+++ aulp-static-pages/pages/index.php
@@ -0,0 +1,18 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+include_once $_SERVER['DOCUMENT_ROOT'] . "/engine/start.php";
+
+    $params = array(
+        'title' => 'Aulp',
+        'content' => 'Aulp home text',
+        'filter' => '',);
+    
+    $body = elgg_view_layout('content', $params);
+    
+    echo elgg_view_page('Aulp', $body);
--- aulp-static-pages/start.php
+++ aulp-static-pages/start.php
@@ -9,12 +9,14 @@
 elgg_register_event_handler('init', 'system', 'aulp_static_pages_init');
 
 function aulp_static_pages_init(){
-    # Add new menu item 'AULP' to point to home page
+    // Add new menu item 'AULP' to point to home page
     elgg_register_menu_item('site', array('name' => 'aulp', 'text' => 'AULP', 'href' => elgg_get_site_url()));
     
-    elgg_register_page_handler('', 'home_page_handler');
+    // Register plugin hook to render home page
+    elgg_register_plugin_hook_handler('index', 'system', 'aulp_home');
 }
 
-function home_page_handler(){
-    
+function aulp_home(){
+    include_once $_SERVER['DOCUMENT_ROOT'] . "/mod/aulp-static-pages/pages/index.php";
+    return true;
 }
